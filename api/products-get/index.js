1203362a63c659f12a652f13c3d617159cd496cd
Kevin Terlesky <Coder-Kev@users.noreply.github.com>
2021-09-28 16:40:39 -0700

    add comment to trigger build

--- api/products-get/index.js
+++ api/products-get/index.js
@@ -1,5 +1,7 @@
 const data = require('../shared/product-data');
+//
 // reference: https://docs.microsoft.com/en-us/learn/modules/publish-static-web-app-api-preview-url/
+// stuck on published staging branch build
 module.exports = async function (context, req) {
   try {
     const products = data.getProducts();
