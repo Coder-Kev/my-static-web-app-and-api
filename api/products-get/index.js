f72c5789162235ec62b3960dcc1a100b6036b941
Kevin Terlesky <Coder-Kev@users.noreply.github.com>
2021-09-28 16:16:42 -0700

    comment to trigger pull request

--- api/products-get/index.js
+++ api/products-get/index.js
@@ -1,5 +1,5 @@
 const data = require('../shared/product-data');
-
+// reference: https://docs.microsoft.com/en-us/learn/modules/publish-static-web-app-api-preview-url/
 module.exports = async function (context, req) {
   try {
     const products = data.getProducts();
